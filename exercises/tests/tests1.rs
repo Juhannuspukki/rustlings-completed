a406d20de9212453520aea6024b65d3c5a1db7bf
Juhannuspukki <jere@jere.pro>
2020-11-04 23:21:46 +0200

    Initial commit

--- exercises/tests/tests1.rs
+++ exercises/tests/tests1.rs
@@ -6,12 +6,21 @@
 // This test has a problem with it -- make the test compile! Make the test
 // pass! Make the test fail! Execute `rustlings hint tests1` for hints :)
 
-// I AM NOT DONE
+pub fn bigger(a: i32, b: i32) -> i32 {
+    // Complete this function to return the bigger number!
+    // Do not use:
+    // - another function call
+    // - additional variables
+    // Execute `rustlings hint if1` for hints
+    return if a > b { a } else { b };
+}
 
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
     fn you_can_assert() {
-        assert!();
+        assert_eq!(10, bigger(10, 8));
     }
 }
--- exercises/tests/tests2.rs
+++ exercises/tests/tests2.rs
@@ -2,12 +2,21 @@
 // This test has a problem with it -- make the test compile! Make the test
 // pass! Make the test fail! Execute `rustlings hint tests2` for hints :)
 
-// I AM NOT DONE
+pub fn bigger(a: i32, b: i32) -> i32 {
+    // Complete this function to return the bigger number!
+    // Do not use:
+    // - another function call
+    // - additional variables
+    // Execute `rustlings hint if1` for hints
+    return if a > b { a } else { b };
+}
 
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
-    fn you_can_assert_eq() {
-        assert_eq!();
+    fn you_can_assert_eq()  {
+        assert_eq!(42, bigger(32, 42));
     }
 }
